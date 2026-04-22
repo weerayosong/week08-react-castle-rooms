fcabda3a84450fc95fe0b48544e323fa7b2ea6b1
weerayosong <weerayosong@outlook.com>
2026-04-22 16:03:43 +0700

    change api endpoint to pokemon sample

--- src/examples/async/SimpleAsyncAwait.jsx
+++ src/examples/async/SimpleAsyncAwait.jsx
@@ -1,25 +1,24 @@
 import { useState, useEffect } from 'react';
 
 export default function PokemonList() {
-    // 1.  useState 151 pokemon Gen1 as Array
     const [pokemonList, setPokemonList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const getData = async () => {
             try {
-                // 2. useEffect ?limit=151
+                // 1. Use pokemon-simple-API
                 const res = await fetch(
-                    'https://pokeapi.co/api/v2/pokemon?limit=151',
+                    'https://pokemon-simple.vercel.app/api/pokemon/?limit=151',
                 );
                 const data = await res.json();
 
-                // data.results
-                setPokemonList(data.results);
+                // 2. API >> []
+                setPokemonList(data);
             } catch (error) {
                 console.error(`can't fetch:`, error);
             } finally {
-                setIsLoading(false); // toggle isLoading to false; it's complete!
+                setIsLoading(false);
             }
         };
 
@@ -32,33 +31,27 @@ export default function PokemonList() {
                 I 💖 151 Pokemon Gen 1
             </h2>
 
-            {/* 3. check (useState) of Loading */}
             {isLoading ? (
                 <p className="text-center text-xl">Catching them all... 😈</p>
             ) : (
-                /* 4. Grid L6 M4 S3 2  */
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-                    {/* 5. map out 151 pokemon 0-150 */}
-                    {pokemonList.map((pokemon, index) => {
-                        // console.log(index);
-                        // ID 1-151
-                        const pokeId = index + 1;
-
-                        // not fetch images >> use image url with ${pokeId}
-                        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`;
-
+                <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-4">
+                    {/* 3. pokemon.id จาก API ได้เลย */}
+                    {pokemonList.map((pokemon) => {
                         return (
                             <div
-                                key={pokemon.name}
+                                // id simple
+                                key={pokemon.id}
                                 className="flex flex-col items-center p-4 bg-lime-700 rounded-xl hover:bg-lime-400 transition duration-300"
                             >
                                 <img
-                                    src={imageUrl}
+                                    // เรียกใช้ pokemon.image จาก API ได้เลย
+                                    src={pokemon.image}
                                     alt={pokemon.name}
                                     className="w-24 h-24 drop-shadow-md"
                                 />
                                 <span className="text-gray-400 text-sm mt-1">
-                                    #{pokeId}
+                                    {/* เรียกใช้ pokemon.id จาก API ได้เลย */}#
+                                    {pokemon.id}
                                 </span>
                                 <p className="capitalize font-semibold text-lg">
                                     {pokemon.name}
